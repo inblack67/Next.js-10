599d4d1db252f639008a328ed1aebafaa45a2472
Aman Bhardwaj <inblack>
2020-11-21 09:34:53 +0530

    new image component

--- src/pages/image.page.tsx
+++ src/pages/image.page.tsx
@@ -1,10 +1,20 @@
-
+import Image from 'next/image';
 
 const image = () =>
 {
     return (
         <div>
-            <h1>image</h1>
+            <h1>images</h1>
+            <p>Fixed</p>
+            <Image src='/images/img1.jpg' layout='fixed' width={ 400 } height={ 400 } />
+            <p>Intrinsic - Can shrink but could never grow beyond the given size</p>
+            <Image src='/images/img2.jpg' layout='intrinsic' width={ 400 } height={ 400 } />
+            <p>Responsive</p>
+            <Image src='/images/img3.jpg' layout='responsive' width={ 400 } height={ 400 } />
+            <p>Fill</p>
+            <div style={ { width: 200, height: 200, position: 'relative' } }>
+                <Image src='/images/img1.jpg' layout='fill' />
+            </div>
         </div>
     );
 };
